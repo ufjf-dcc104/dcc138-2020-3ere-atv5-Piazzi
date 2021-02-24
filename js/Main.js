ff631a46c2f159709952a99dc26ff7ff6704c46b
Lucas Piazzi <lpiazzi26@gmail.com>
2021-02-24 16:42:27 -0300

    Adiciona lista de sprites na Scene

--- js/Main.js
+++ js/Main.js
@@ -2,12 +2,13 @@ import Scene from "./Scene.js";
 import Sprite from "./Sprite.js";
 
 const canvas = document.querySelector("canvas");
-const ctx = canvas.getContext("2d");
 const scene1 = new Scene(canvas);
-scene1.draw();
-
 const pc = new Sprite({});
 const en1 = new Sprite({x:140, w:30, color:"red"});
 
-pc.draw(ctx);
-en1.draw(ctx);
+
+scene1.add(pc);
+scene1.add(en1);
+
+scene1.draw();
+
