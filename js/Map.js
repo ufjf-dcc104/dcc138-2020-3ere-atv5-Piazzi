10a0ac5a9c47d4054ef12f935f921d79c11255a3
Lucas Piazzi <lpiazzi26@gmail.com>
2021-03-06 16:52:14 -0300

    Simplifica desenho do mapa

--- js/Map.js
+++ js/Map.js
@@ -21,25 +21,22 @@ export default class Map {
         switch (this.tiles[r][c]) {
           case 1:
             ctx.fillStyle = "grey";
-            ctx.fillRect(c * this.SIZE, r * this.SIZE, this.SIZE, this.SIZE);
             ctx.lineWidth = 1;
             ctx.strokeStyle = "black";
-            ctx.strokeRect(c * this.SIZE, r * this.SIZE, this.SIZE, this.SIZE);
             break;
           case 2:
             ctx.fillStyle = "red";
-            ctx.fillRect(c * this.SIZE, r * this.SIZE, this.SIZE, this.SIZE);
-            ctx.lineWidth = 1;
+            ctx.lineWidth = 2;
             ctx.strokeStyle = "orange";
-            ctx.strokeRect(c * this.SIZE, r * this.SIZE, this.SIZE, this.SIZE);
             break;
           default:
             ctx.fillStyle = "black";
-            ctx.fillRect(c * this.SIZE, r * this.SIZE, this.SIZE, this.SIZE);
             ctx.lineWidth = 1;
             ctx.strokeStyle = "grey";
-            ctx.strokeRect(c * this.SIZE, r * this.SIZE, this.SIZE, this.SIZE);
         }
+        ctx.fillRect(c * this.SIZE, r * this.SIZE, this.SIZE, this.SIZE);
+        ctx.strokeRect(c * this.SIZE, r * this.SIZE, this.SIZE, this.SIZE);
+
       }
     }
   }
